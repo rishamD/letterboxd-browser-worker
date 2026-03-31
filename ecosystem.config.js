867acff140afd4ce321c2572cad7e77fde56fb67
Rishamdeep Singh <rishamdeep44@gmail.com>
2026-03-31 17:21:29 -0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   apps: [{
     name: 'browser-worker',
-    script: 'server.js',
+    script: 'src/server.js', // Updated path
     instances: 1,
     exec_mode: 'fork',
     env_production: {
       NODE_ENV: 'production',
       PORT: 8081,
-      // PM2 will grab PROXY_LIST from the shell
       PROXY_LIST: process.env.PROXY_LIST 
     }
   }]
